3a8ed11ab4bdfd37d034db76df86511c9feff1a1
priya singh <priya955singh@gmail.com>
2024-05-24 20:02:10 +0530

    add css

--- src/components/PhnImgCard.jsx
+++ src/components/PhnImgCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PhnImgCard = ({id, img, title, para}) => {
   return (
-    <div key={id} className='w-[166px] h-[301px] bg-black rounded-2xl mt-20 md:mt-10'>
+    <div key={id} className='w-[166px] h-[301px] bg-black-gray-gradient rounded-2xl mt-20 md:mt-10'>
     <div className='relative w-full h-[60%] flex flex-col justify-center items-center'>
          <div className='absolute top-[-70px] w-40'>
 
